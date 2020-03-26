4bae89e436eee24f2fefaac2a6a06f858b69c7d8
tarquin-the-brave <tomsteavenson@gmail.com>
2020-03-26 20:43:25 +0000

    add test for json yaml incompatability

--- src/main.rs
+++ src/main.rs
@@ -110,6 +110,16 @@ fn main() -> anyhow::Result<()> {
         &std::fs::read_to_string("example.schema.json")?,
     )?;
 
+    let some_yaml = r#"
+        [5,6]: true
+        "#;
+
+    let try_yaml = serde_yaml::from_str::<serde_yaml::Value>(some_yaml);
+    let try_json = serde_yaml::from_str::<serde_json::Value>(some_yaml);
+
+    assert!(try_yaml.is_ok());
+    assert!(try_json.is_err());
+
     println!("All good!");
 
     Ok(())
